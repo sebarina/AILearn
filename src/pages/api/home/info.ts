54da81b5d6ca7dbf9754ea80f596a5935163d485
Sebarina <xuling1987@gmail.com>
2025-05-21 14:12:43 +0800

    fix build break

--- src/pages/api/home/info.ts
+++ src/pages/api/home/info.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 
--- src/pages/api/image/analysis.ts
+++ src/pages/api/image/analysis.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
