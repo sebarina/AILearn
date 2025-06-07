526200f1c39a0d3ff14318bbcf7d78403201ea92
Sebarina <xuling1987@gmail.com>
2025-06-07 17:32:51 +0800

    add images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: false,
 
     images: {
-    domains: ['r2.flowith.net'],
+    domains: ['r2.flowith.net','r2.icool-test.xin'],
   },
 };
 
---
+++ src/pages/api/text/comment.ts
@@ -0,0 +1,46 @@
+export const runtime = 'edge';
+
+export default async function handler(req:any) {
+    
+    try {
+        const body = await req.json();
+        const text = body.text;
+       
+        const response = await fetch(`https://api.coze.cn/v1/workflow/run`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization':"Bearer pat_GvOP7slpNQo4YZ89NaMFvzsDcVU9Yj5AJsIbYPysBRD5l6O9iceF6sq6G5WLV1Me"
+            },
+            body: JSON.stringify({
+                "app_id": "7506692414422171698",
+				"workflow_id":"7506711692366676031",
+				"parameters":{
+					"input":text,
+            
+				}
+            })
+         })
+         const data = await response.json()
+         console.log(data);
+         if (data.code == 0) {
+            return new Response(JSON.stringify({"code":200, message: '成功', "data":data.data}), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+         } else {
+            
+            return new Response(JSON.stringify({"code":500, message: '请求失败'}), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+           
+         }
+
+        
+
+    } catch (error) {
+        console.log(error);
+        return new Response(JSON.stringify({"code":501, message: '请求异常'}), {
+                headers: { 'Content-Type': 'application/json' },
+        });
+    }    
+}
